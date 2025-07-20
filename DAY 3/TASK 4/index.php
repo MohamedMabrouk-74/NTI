ff482306dbf23f7732815e4d4d30f066ed0a69e7
Mohamedmabrouk74 <155705467+Mohamedmabrouk74@users.noreply.github.com>
2025-07-20 04:04:51 +0300

    .

---
+++ DAY 3/TASK 4/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="css/bootstrap.css" rel="stylesheet">
+</head>
+<body  style="background-color: aquamarine">
+<div class="container">
+<div class="row mt-4 p-5 justify-content-center">
+<div class="col-lg-4">
+    
+<form class="w-100 bg-light p-5">
+    <h2 class="text-center mn-4">User</h2>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+   
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email</label>
+    <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"></div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Age</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+   
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">City</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+   
+  </div>
+  <button type="submit" class="btn btn-primary" style="width:100%">Submit</button>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>
